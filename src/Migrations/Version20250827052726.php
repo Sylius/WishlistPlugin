75da3d29f33fe2a57d380a94fd3d5abbb64cad4e
PiotrSzymanski2000 <PiotrekSz-18@wp.pl>
2025-08-27 10:27:47 +0200

    [Maintenance] fix default 1 value for quantity

--- src/Migrations/Version20250827052726.php
+++ src/Migrations/Version20250827052726.php
@@ -25,7 +25,7 @@ final class Version20250827052726 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE sylius_wishlist_product ADD quantity INT NOT NULL');
+        $this->addSql('ALTER TABLE sylius_wishlist_product ADD quantity INT NOT NULL DEFAULT 1');
     }
 
     public function down(Schema $schema): void
