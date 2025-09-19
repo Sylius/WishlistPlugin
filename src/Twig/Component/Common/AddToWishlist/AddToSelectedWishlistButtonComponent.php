5e0f77425000d23370c7249105fa6a7841645614
PiotrSzymanski2000 <PiotrekSz-18@wp.pl>
2025-09-19 11:40:19 +0200

    [Maintenance] cr fixes

--- src/Twig/Component/Common/AddToWishlist/AddToSelectedWishlistButtonComponent.php
+++ src/Twig/Component/Common/AddToWishlist/AddToSelectedWishlistButtonComponent.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Sylius\WishlistPlugin\Twig\Component\Common\AddToWishlist;
 
 use Sylius\Bundle\UiBundle\Twig\Component\TemplatePropTrait;
+use Sylius\Bundle\ShopBundle\Twig\Component\Product\Trait\ProductLivePropTrait;
 use Sylius\Component\Core\Model\ProductInterface;
 use Sylius\Component\Core\Repository\ProductRepositoryInterface;
 use Sylius\TwigHooks\LiveComponent\HookableLiveComponentTrait;
@@ -32,36 +33,34 @@ final class AddToSelectedWishlistButtonComponent
     use DefaultActionTrait;
     use TemplatePropTrait;
     use HookableLiveComponentTrait;
+    use ProductLivePropTrait;
 
-    #[LiveProp]
-    public int $wishlistId;
-
-    #[LiveProp]
-    public int $productId;
+    #[LiveProp(hydrateWith: 'hydrateWishlist', dehydrateWith: 'dehydrateWishlist')]
+    public ?WishlistInterface $wishlist = null;
 
     public function __construct(
         private readonly WishlistRepositoryInterface $wishlistRepository,
-        private readonly ProductRepositoryInterface $productRepository,
+        ProductRepositoryInterface $productRepository,
         private readonly RequestStack $requestStack,
         private readonly TranslatorInterface $translator,
         private readonly UrlGeneratorInterface $urlGenerator,
         private readonly MessageBusInterface $commandBus,
     ) {
+        $this->initializeProduct($productRepository);
     }
 
     #[LiveAction]
     public function add(): RedirectResponse
     {
-        /** @var ?WishlistInterface $wishlist */
-        $wishlist = $this->wishlistRepository->find($this->wishlistId);
+        $wishlist = $this->wishlist;
         if (null === $wishlist) {
             throw new WishlistNotFoundException('Wishlist not found.');
         }
 
         /** @var ?ProductInterface $product */
-        $product = $this->productRepository->find($this->productId);
+        $product = $this->product;
         if (null === $product) {
-            throw new ProductNotFoundException(sprintf('The Product %s does not exist', $this->productId));
+            throw new ProductNotFoundException('Product not found.');
         }
 
         $this->commandBus->dispatch(new AddProductToSelectedWishlist($wishlist, $product));
@@ -72,8 +71,21 @@ final class AddToSelectedWishlistButtonComponent
 
         return new RedirectResponse(
             $this->urlGenerator->generate('sylius_wishlist_plugin_shop_locale_wishlist_show_chosen_wishlist', [
-                'wishlistId' => $this->wishlistId,
+                'wishlistId' => (int) $wishlist->getId(),
             ]),
         );
+    }
+
+    public function hydrateWishlist(mixed $value): ?WishlistInterface
+    {
+        /** @var WishlistInterface|null $wishlist */
+        $wishlist = $this->wishlistRepository->find($value);
+
+        return $wishlist;
+    }
+
+    public function dehydrateWishlist(?WishlistInterface $wishlist): mixed
+    {
+        return $wishlist?->getId();
     }
 }
